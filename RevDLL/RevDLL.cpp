2e41ce4d1403c9325ebab9784dae6d539ed9502a
link-chen <Link_Chen@trendmicro.com>
2019-03-07 15:25:36 +0800

    [+] Add RevDLL Sln

---
+++ RevDLL/RevDLL.cpp
@@ -0,0 +1,13 @@
+// RevDLL.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+
+BOOL WINAPI DllMain(
+	_In_ HINSTANCE hinstDLL,
+	_In_ DWORD     fdwReason,
+	_In_ LPVOID    lpvReserved
+) {
+
+}
