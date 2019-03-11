09d3148632068deb4c57955e7bd41de0071192d9
link <779842935@qq.com>
2019-03-12 00:22:11 +0800

    Add share memory at dll

--- RevDLL/dllmain.cpp
+++ RevDLL/dllmain.cpp
@@ -1,6 +1,27 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
+#include"../WinRev/Common.h"
 
+void AttachProcess() {
+	char input[18] = { 0 };
+	puts("Emmm? you now the answer?");
+	scanf("%17s", input);
+	// here we write the buffer to share memory, and wen send the 
+	// really event to the main thread to make main thread
+	const int dwSize = 0x8000;
+	HANDLE hSharemem = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, NULL, dwSize, SHARE_MEMORY);
+	if (hSharemem == NULL) {
+		printf("Create File failed!\n");
+		return;
+	}
+	void* buffer = MapViewOfFile(hSharemem, FILE_MAP_WRITE, 0, 0, dwSize);
+	if (buffer == NULL) {
+		printf("Map failed\n");
+		CloseHandle(hSharemem);
+	}
+	memcpy(buffer, input, sizeof(input));
+	// end 
+}
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -8,7 +29,9 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 {
     switch (ul_reason_for_call)
     {
-    case DLL_PROCESS_ATTACH:
+	case DLL_PROCESS_ATTACH: {
+
+	}
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
