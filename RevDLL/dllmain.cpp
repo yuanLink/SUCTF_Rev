f8573897d30141a96f0aa775f31fdd9143be2b0c
link-chen <Link_Chen@trendmicro.com>
2019-03-13 09:53:16 +0800

    add dll - main share memory and event

--- RevDLL/dllmain.cpp
+++ RevDLL/dllmain.cpp
@@ -2,25 +2,57 @@
 #include "stdafx.h"
 #include"../WinRev/Common.h"
 
+void DecMessage(char key[], char answer[]) {
+	// TODO: use DES to decrypt this message
+	// and final it will get a key with length 18
+}
 void AttachProcess() {
 	char input[18] = { 0 };
 	puts("Emmm? you now the answer?");
 	scanf("%17s", input);
 	// here we write the buffer to share memory, and wen send the 
 	// really event to the main thread to make main thread
-	const int dwSize = 0x8000;
-	HANDLE hSharemem = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, NULL, dwSize, SHARE_MEMORY);
+	// we wait main thread to
+	HANDLE hEvent = OpenEvent(EVENT_ALL_ACCESS, TRUE, DLL_INPUT);
+	if (hEvent == NULL) {
+		printf("[Error] Create Event ERROR\n");
+		return;
+	}
+	// set the event to main thread
+	WaitForSingleObject(hEvent, -1);
+	CloseHandle(hEvent);
+	HANDLE hSharemem = OpenFileMapping(FILE_MAP_READ, NULL, SHARE_MEMORY);
 	if (hSharemem == NULL) {
 		printf("Create File failed!\n");
 		return;
 	}
-	void* buffer = MapViewOfFile(hSharemem, FILE_MAP_WRITE, 0, 0, dwSize);
+	void* buffer = MapViewOfFile(hSharemem, FILE_MAP_READ, 0, 0, g_dwSize);
 	if (buffer == NULL) {
 		printf("Map failed\n");
 		CloseHandle(hSharemem);
+		return;
 	}
-	memcpy(buffer, input, sizeof(input));
-	// end 
+	CloseHandle(hSharemem);
+	char* ans_buffer = (char*)malloc(g_dwSize);
+	memcpy(ans_buffer, buffer, sizeof(g_dwSize));
+	// now finish, we wait for main thread finish 
+	// here we use DES to decrypt the solution
+	char key[] = "Ak13aK3y";
+	char answer[18] = { 0 };
+	DecMessage(key, answer);
+	if (!strcmp(answer, input)) {
+		printf("Get finally answer!\n");
+	}
+	else {
+		printf("wow... game start!\n");
+	}
+
+	// here we will finish all the process
+#ifdef _DEBUG
+	return ;
+#else
+	exit(0);
+#endif
 }
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -30,7 +62,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
 	case DLL_PROCESS_ATTACH: {
-
+		AttachProcess();
 	}
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
