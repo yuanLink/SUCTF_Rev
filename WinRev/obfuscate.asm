8e9a715f0a4aac5595912e2f6584a8e1a3615825
link <779842935@qq.com>
2019-04-19 00:01:15 +0800

    [WIP] Add obfuscation to some code
    Now just add asm to VS, but we could not use EMIT_ Macro

---
+++ WinRev/obfuscate.asm
@@ -0,0 +1,29 @@
+.code
+Obfus proc
+	call fake1
+fake1:
+	pop rax
+	; mov p,eax // TODO:add a offset variable to check
+	call true1
+	db 0xEA
+	jmp fake2
+true1:
+	pop rbx
+	inc rbx
+	push rbx
+	mov rax, 54536566h
+	ret
+fake2:
+	call true2
+	mov rbx, 55694855h
+	jmp ed
+true2:
+	mov rbx, 51525354h
+	pop rbx
+	mov rbx, offset ed
+	push rbx
+	ret
+ed:
+	mov rbx, 43454647h
+Obfus endp
+end
